5a84247efc5abab0913a9737cb37b75c71e34f61
par.dahlman <par.dahlman@valtech.se>
2015-01-21 22:48:07 +0100

    Add ClientUtils helper class
    That can be used to set stuff for the client, such as UserAgent.

---
+++ PainlessHttp.Tests/Utils/ClientUtilsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PainlessHttp.Utils;
+
+namespace PainlessHttp.Tests.Utils
+{
+	[TestFixture]
+	public class ClientUtilsTests
+	{
+		public class When_Calling_GetUserAgent
+		{
+			[Test]
+			public void ShouldReturnExpectedAgentName()
+			{
+				/* Setup */
+				const string version = "1.0.0.0";
+				var expected = string.Format("Painless Http Client {0}", version);
+				/* Test */
+				var agent = ClientUtils.GetUserAgent();
+
+				/* Assert */
+				Assert.That(agent, Is.EqualTo(expected));
+			}
+		}
+	}
+}
---
+++ PainlessHttp/Utils/ClientUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using PainlessHttp.Client;
+
+namespace PainlessHttp.Utils
+{
+	public class ClientUtils
+	{
+		private const string _painlessAgent = "Painless Http Client";
+		private static Version _version;
+		public static string GetUserAgent()
+		{
+			if (_version == null)
+			{
+				_version = Assembly.GetAssembly(typeof (HttpClient)).GetName().Version;
+			}
+			return string.Format("{0} {1}", _painlessAgent, _version);
+		}
+	}
+}
