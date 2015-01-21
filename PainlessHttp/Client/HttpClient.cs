ec079f65d9546143dc3d6e4c0d9aefa237d75b74
par.dahlman <par.dahlman@valtech.se>
2015-01-21 20:47:31 +0100

    Implement basic Get
    This hard-wired not safe guarded code is added just to have something to
    verify the comming 'Tools', which will be a sandbox.

--- PainlessHttp/Client/HttpClient.cs
+++ PainlessHttp/Client/HttpClient.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
+using PainlessHttp.Client.Configuration;
 using PainlessHttp.Client.Contracts;
 using PainlessHttp.Http;
 using PainlessHttp.Http.Contracts;
@@ -8,9 +11,35 @@ namespace PainlessHttp.Client
 {
 	public class HttpClient : IHttpClient
 	{
-		public IHttpResponse Get(string url, ContentType type, object query = null)
+		private readonly HttpClientConfiguration _config;
+
+		public HttpClient(HttpClientConfiguration config)
 		{
-			throw new NotImplementedException();
+			_config = config;
+		}
+
+		public IHttpResponse Get(string url, ContentType type = ContentType.ApplicationJson, object query = null)
+		{
+			var httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Format("{0}{1}", _config.BaseUrl, url));
+			httpWebRequest.AllowAutoRedirect = true;
+			httpWebRequest.ContentType = "application/json";
+			httpWebRequest.UserAgent = "Painless Http Client";
+			httpWebRequest.Method = "GET";
+
+			var response = (HttpWebResponse)httpWebRequest.GetResponse();
+			var responseStream = response.GetResponseStream();
+
+			string raw;
+			using (var reader = new StreamReader(responseStream))
+			{
+				raw = reader.ReadToEnd();
+			}
+
+			return new HttpResponse
+			{
+				StatusCode = response.StatusCode,
+				RawContent = raw
+			};
 		}
 
 		public IHttpResponse<T> Get<T>(string url, ContentType type, object query = null)
