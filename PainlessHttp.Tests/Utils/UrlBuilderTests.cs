7dbf6784c3dd300115093d9f9500fc49489e2da4
par.dahlman <par.dahlman@valtech.se>
2015-01-21 23:51:05 +0100

    Add UrlBuilder that supports paths and queryparams

---
+++ PainlessHttp.Tests/Utils/UrlBuilderTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using PainlessHttp.Utils;
+
+namespace PainlessHttp.Tests.Utils
+{
+	[TestFixture]
+	public class UrlBuilderTests
+	{
+		private UrlBuilder _builder;
+		private const string _baseUrl = "https://www.github.com";
+
+		[SetUp]
+		public void Setup()
+		{
+			_builder = new UrlBuilder(_baseUrl);
+		}
+
+		[Test]
+		public void Should_Return_Url_That_Starts_With_Base_Url()
+		{
+			/* Setup */
+			/* Test */
+			var result = _builder.Build();
+
+			/* Assert */
+			Assert.That(result, Is.StringStarting(_baseUrl));
+		}
+
+		[Test]
+		public void Should_Add_Relative_Path_To_Base_Url()
+		{
+			/* Setup */
+			const string relative = "/pardahlman";
+			var expected = string.Format("{0}{1}", _baseUrl, relative);
+
+			/* Test */
+			var result = _builder.Build(relative);
+
+			/* Assert */
+			Assert.That(result, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Should_Add_Query_Parameters()
+		{
+			/* Setup */
+			var query = new {firstParam = "firstValue", secondParam = "secondValue"};
+			var expected = string.Format("{0}?firstParam=firstValue&secondParam=secondValue", _baseUrl);
+
+			/* Test */
+			var result = _builder.Build(string.Empty, query);
+
+			/* Assert */
+			Assert.That(result, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void Should_Be_Able_To_Handle_Relative_Url_And_Query()
+		{
+			/* Setup */
+			const string relative = "/pardahlman";
+			var query = new { id = 1};
+			var expected = string.Format("{0}{1}?id=1", _baseUrl, relative);
+
+			/* Test */
+			var result = _builder.Build(relative, query);
+
+			/* Assert */
+			Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+}
---
+++ PainlessHttp/Utils/UrlBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PainlessHttp.Utils
+{
+	public class UrlBuilder
+	{
+		private readonly string _baseUrl;
+
+		public UrlBuilder(string baseUrl)
+		{
+			_baseUrl = baseUrl;
+		}
+
+		public string Build(string relativeUrl = null, object query = null)
+		{
+			var builder = new StringBuilder(_baseUrl);
+			if (relativeUrl != null)
+			{
+				builder.Append(relativeUrl);
+			}
+			if (query != null)
+			{
+				builder.Append(CreateQueryString(query));
+			}
+			return builder.ToString();
+		}
+
+		private static string CreateQueryString(object query)
+		{
+			var queryType = query.GetType();
+			var properties = new List<PropertyInfo>(queryType.GetProperties());
+			
+			var queryString = properties
+				.Select(p => p.Name + "=" + p.GetValue(query))
+				.Aggregate((accumilated, delta) => string.Format("{0}&{1}", accumilated, delta))
+				.Insert(0, "?");
+
+			return queryString;
+		}
+	}
+}
