e9ca93c92ef13dd2e8383fe18e45c5529f750431
par.dahlman <par.dahlman@valtech.se>
2015-01-21 22:26:10 +0100

    Add ContentTypes const class
    That holds all content types

---
+++ PainlessHttp/Http/ContentTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace PainlessHttp.Http
+{
+	public static class ContentTypes
+	{
+		public const string TextPlain = "text/plain";
+		public const string TextHtml = "text/html";
+		public const string TextCsv = "text/csv";
+		public const string ApplicationJson = "application/json";
+		public const string ApplicationXml = "application/xml";
+	}
+}
