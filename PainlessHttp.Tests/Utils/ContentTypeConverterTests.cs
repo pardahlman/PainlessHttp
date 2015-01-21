c5c787a1fd6e6ae3f9838ef338124f479e40caff
par.dahlman <par.dahlman@valtech.se>
2015-01-21 22:34:44 +0100

    Add ContentTypeConverter
    That helps us convert the enum to strings and right back to enum if we
    want to.

---
+++ PainlessHttp.Tests/Utils/ContentTypeConverterTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+using PainlessHttp.Http;
+using PainlessHttp.Utils;
+
+namespace PainlessHttp.Tests.Utils
+{
+	public class ContentTypeConverterTests
+	{
+		[Test]
+		public void ShouldThrowExceptionIfContentTypeIsUnknown()
+		{
+			/* Setup */
+			var type = ContentType.Unknown;
+
+			/* Test & Assert */
+			Assert.Throws<ArgumentException>(() => ContentTypeConverter.ConvertToString(type));
+		}
+
+		[Test]
+		public void ShouldThrowExceptionIfStringDoesNotMatchContentType()
+		{
+			/* Setup */
+			const string nonMatch = "This is not a content type";
+
+			/* Test & Assert */
+			Assert.Throws<ArgumentException>(() => ContentTypeConverter.ConvertToEnum(nonMatch));
+		}
+
+		[TestCase(ContentType.ApplicationJson, ContentTypes.ApplicationJson)]
+		[TestCase(ContentType.ApplicationXml, ContentTypes.ApplicationXml)]
+		[TestCase(ContentType.TextPlain, ContentTypes.TextPlain)]
+		[TestCase(ContentType.TextCsv, ContentTypes.TextCsv)]
+		[TestCase(ContentType.TextHtml, ContentTypes.TextHtml)]
+		public void ShouldConvertEnumsToStrings(ContentType type, string expectedResult)
+		{
+			/* Setup */
+			/* Test */
+			var result = ContentTypeConverter.ConvertToString(type);
+
+			/* Assert */
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase(ContentTypes.ApplicationJson, ContentType.ApplicationJson)]
+		[TestCase(ContentTypes.ApplicationXml, ContentType.ApplicationXml)]
+		[TestCase(ContentTypes.TextPlain, ContentType.TextPlain)]
+		[TestCase(ContentTypes.TextCsv, ContentType.TextCsv)]
+		[TestCase(ContentTypes.TextHtml, ContentType.TextHtml)]
+		public void ShouldConvertStringToEnum(string type, ContentType expectedResult)
+		{
+			/* Setup */
+			/* Test */
+			var result = ContentTypeConverter.ConvertToEnum(type);
+
+			/* Assert */
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+	}
+}
---
+++ PainlessHttp/Utils/ContentTypeConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PainlessHttp.Http;
+
+namespace PainlessHttp.Utils
+{
+	public class ContentTypeConverter
+	{
+
+		static readonly List<Tuple<ContentType, string>> ContentTypes = new List<Tuple<ContentType, string>>
+		{
+			new Tuple<ContentType, string>(ContentType.ApplicationJson, Http.ContentTypes.ApplicationJson),
+			new Tuple<ContentType, string>(ContentType.ApplicationXml, Http.ContentTypes.ApplicationXml),
+			new Tuple<ContentType, string>(ContentType.TextCsv, Http.ContentTypes.TextCsv),
+			new Tuple<ContentType, string>(ContentType.TextHtml, Http.ContentTypes.TextHtml),
+			new Tuple<ContentType, string>(ContentType.TextPlain, Http.ContentTypes.TextPlain),
+		};
+	
+		public static string ConvertToString(ContentType type)
+		{
+			var result =  ContentTypes
+								.Where(ct => ct.Item1 == type)
+								.Select(ct => ct.Item2)
+								.FirstOrDefault();
+
+			if (!string.IsNullOrWhiteSpace(result))
+			{
+				return result;
+			}
+
+			throw new ArgumentException(string.Format("Unable to convert content type {0} to string.", type));
+		}
+
+		public static ContentType ConvertToEnum(string type)
+		{
+			var result = ContentTypes
+								.Where(ct => ct.Item2 == type)
+								.Select(ct => ct.Item1)
+								.FirstOrDefault();
+
+			if (result != ContentType.Unknown)
+			{
+				return result;
+			}
+
+			throw new ArgumentException(string.Format("Unable to convert content type {0} to enum.", type));
+		}
+	}
+}
