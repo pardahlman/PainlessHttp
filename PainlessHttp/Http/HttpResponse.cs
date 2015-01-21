627c91a7d196aff1d9a01e40f705a5eb7820d3be
par.dahlman <par.dahlman@valtech.se>
2015-01-21 20:45:36 +0100

    Add HttpResponse class

---
+++ PainlessHttp/Http/HttpResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using PainlessHttp.Http.Contracts;
+
+namespace PainlessHttp.Http
+{
+	public class HttpResponse : IHttpResponse
+	{
+		public HttpStatusCode StatusCode { get; set; }
+		public string RawContent { get; set; }
+	}
+}
