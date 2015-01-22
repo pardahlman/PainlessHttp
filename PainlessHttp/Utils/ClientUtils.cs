aff33d80506feec7ff68c455f1ab42685e84787f
par.dahlman <par.dahlman@valtech.se>
2015-01-22 01:51:11 +0100

    Add methods to read and parse response body
    This is done async and is currently hard-wired to the JsonSerializer. Here,
    we could make sure that we support different types of content type as well
    as making the serializers plugable.

--- PainlessHttp/Utils/ClientUtils.cs
+++ PainlessHttp/Utils/ClientUtils.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using PainlessHttp.Client;
 
 namespace PainlessHttp.Utils
@@ -8,6 +14,7 @@ namespace PainlessHttp.Utils
 	{
 		private const string _painlessAgent = "Painless Http Client";
 		private static Version _version;
+
 		public static string GetUserAgent()
 		{
 			if (_version == null)
@@ -15,6 +22,33 @@ namespace PainlessHttp.Utils
 				_version = Assembly.GetAssembly(typeof (HttpClient)).GetName().Version;
 			}
 			return string.Format("{0} {1}", _painlessAgent, _version);
+		}
+
+		public static async Task<string> ReadBodyAsync(HttpWebResponse response)
+		{
+			var responseStream = response.GetResponseStream();
+			if (responseStream == null)
+			{
+				throw new ArgumentNullException("response");
+			}
+
+			string raw;
+			using (var reader = new StreamReader(responseStream))
+			{
+				raw = await reader.ReadToEndAsync();
+			}
+			return raw;
+		}
+		public async static Task<T> ParseBodyAsync<T>(HttpWebResponse response)
+		{
+			var readTask = ReadBodyAsync(response);
+			var deserializeTask = await readTask.ContinueWith(task =>
+							{
+								var body = task.Result;
+								var typedBody = JsonConvert.DeserializeObject<T>(body);
+								return typedBody;
+							});
+			return deserializeTask;
 		}
 	}
 }
