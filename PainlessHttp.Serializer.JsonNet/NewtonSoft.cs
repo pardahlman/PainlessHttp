421eb905fd8b50e1ebc1bd9f44142548e069f1ad
par.dahlman <par.dahlman@valtech.se>
2015-01-23 23:39:43 +0100

    Add project for JsonNet serializer
    That can be pluged-in to HttpClient.

---
+++ PainlessHttp.Serializer.JsonNet/NewtonSoft.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PainlessHttp.Serializer.JsonNet
+{
+	public class NewtonSoft
+	{
+		private static NewtonsoftSettings _settings = new NewtonsoftSettings();
+
+		public static object Deserialize(string data, Type type)
+		{
+			if (_settings.Settings != null)
+			{
+				return JsonConvert.DeserializeObject(data, type, _settings.Settings);
+			}
+			if(_settings.JsonConverters != null)
+			{
+				return JsonConvert.DeserializeObject(data, type, _settings.JsonConverters);
+			}
+
+			return JsonConvert.DeserializeObject(data, type);
+		}
+
+		public static string Serialize(object data)
+		{
+			if (_settings.Settings != null)
+			{
+				return JsonConvert.SerializeObject(data, _settings.Settings);
+			}
+			if (_settings.JsonConverters != null)
+			{
+				return JsonConvert.SerializeObject(data, _settings.Formatting, _settings.JsonConverters);
+			}
+
+			return JsonConvert.SerializeObject(data, _settings.Formatting);
+		}
+
+		
+		public static void UpdateSettings(NewtonsoftSettings settings)
+		{
+			_settings = settings;
+		}
+	}
+}
---
+++ PainlessHttp.Serializer.JsonNet/NewtonsoftSettings.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace PainlessHttp.Serializer.JsonNet
+{
+	public class NewtonsoftSettings
+	{
+		public Formatting Formatting { get; set; }
+		public JsonSerializerSettings Settings { get; set; }
+		public JsonConverter[] JsonConverters { get; set; }
+	}
+}
