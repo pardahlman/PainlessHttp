cf524044ea71d35871dba2ff8b67cba26018ac67
par.dahlman <par.dahlman@valtech.se>
2015-01-23 21:38:14 +0100

    Add Advanced config section for HttpConfiguration
    That holds prop for overriding serializers.

--- PainlessHttp/Client/Configuration/HttpClientConfiguration.cs
+++ PainlessHttp/Client/Configuration/HttpClientConfiguration.cs
@@ -1,7 +1,32 @@
-﻿namespace PainlessHttp.Client.Configuration
+﻿using System.Collections.Generic;
+using PainlessHttp.Serializers.Contracts;
+
+namespace PainlessHttp.Client.Configuration
 {
 	public class HttpClientConfiguration
 	{
 		public string BaseUrl { get; set; }
+		public AdvancedConfiguration Advanced { get; set; }
+
+		public HttpClientConfiguration()
+		{
+			Advanced = new AdvancedConfiguration();
+		}
+		
+
+		public class AdvancedConfiguration
+		{
+			/// <summary>
+			/// Override the default content serializers.
+			/// </summary>
+			public IEnumerable<IContentSerializer> Serializers { get; set; }
+
+			public AdvancedConfiguration()
+			{
+				Serializers = new List<IContentSerializer>();
+			}
+		}
 	}
+
+	
 }
