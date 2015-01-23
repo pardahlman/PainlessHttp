7176e0d4fe6e5432636d6f3486019dac6a135e66
par.dahlman <par.dahlman@valtech.se>
2015-01-23 23:50:33 +0100

    Updated Sandbox

--- PainlessHttp.Sandbox/Program.cs
+++ PainlessHttp.Sandbox/Program.cs
@@ -1,8 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using PainlessHttp.Client;
 using PainlessHttp.Client.Configuration;
 using PainlessHttp.DevServer.Model;
-using PainlessHttp.Serializers;
+using PainlessHttp.Http;
+using PainlessHttp.Serializer.JsonNet;
+using PainlessHttp.Serializers.Contracts;
+using PainlessHttp.Serializers.Custom;
 
 namespace PainlessHttp.Sandbox
 {
@@ -12,16 +20,45 @@ namespace PainlessHttp.Sandbox
 		{
 			var config = new HttpClientConfiguration
 			{
-				BaseUrl = "http://localhost:1337/"
+				BaseUrl = "http://localhost:1337/",
+				Advanced =
+				{
+					Serializers = new List<IContentSerializer>
+					{
+						new Serializer<NewtonSoft>(ContentType.ApplicationJson)
+					}
+				}
 			};
 
-			var client = new HttpClient(config);
-
 			var tomorrow = new Todo { Description = "Sleep in" };
-			var serializer = new DefaultJsonSerializer();
-			var tomorrowJson = serializer.Serialize(tomorrow);
+
+			// Create a custom serializer
+			var custom = SerializeSettings
+				.For(ContentType.ApplicationJson)
+					.Serialize(NewtonSoft.Serialize)
+					.Deserialize(DefaultJson.Deserialize);
 			
-			var created = client.PostAsync<Todo>("api/todos", tomorrowJson).Result;
+			var customJson = custom.Serialize(tomorrow);
+			var customObj = custom.Deserialize<Todo>(customJson);
+
+			// Configure JsonNet however you want
+			var newtonSoftSettings = new JsonSerializerSettings
+			{
+				ContractResolver = new CamelCasePropertyNamesContractResolver()
+			};
+
+			//Register the change
+			NewtonSoft.UpdateSettings(new NewtonsoftSettings {Settings = newtonSoftSettings});
+
+			// Go crazy, oh baby!
+			var serializer = new Serializer<NewtonSoft>(ContentType.ApplicationJson);
+			var tomorrowJson = serializer.Serialize(tomorrow);
+			var tommorowObj = serializer.Deserialize<Todo>(tomorrowJson);
+
+			var client = new HttpClient(config);
+			var created = client.PostAsync<Todo>("api/todos", tomorrow).Result;
 		}
 	}
+
+	
 }
