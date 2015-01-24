d8e2d9793c6d47895248777f92676b6562ea7853
par.dahlman <par.dahlman@valtech.se>
2015-01-24 10:39:57 +0100

    Add integration tests for PUT

---
+++ PainlessHttp.IntegrationTests/Methods/PutTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using NUnit.Framework;
+using PainlessHttp.Client;
+using PainlessHttp.Client.Configuration;
+using PainlessHttp.DevServer.Data;
+using PainlessHttp.DevServer.Model;
+
+namespace PainlessHttp.IntegrationTests.Methods
+{
+	[TestFixture]
+	public class PutTests
+	{
+		private HttpClient _client;
+		private InMemoryTodoRepo _repo;
+
+		[SetUp]
+		public void Setup()
+		{
+			var config = new HttpClientConfiguration
+			{
+				BaseUrl = WebApiSetupFixture.BaseAddress
+			};
+
+			_client = new HttpClient(config);
+			_repo = InMemoryTodoRepo.Instance;
+		}
+
+		[Test]
+		public void Should_Be_Able_To_Put_Object()
+		{
+			/* Setup */
+			const string newDescription = "This is the new description";
+			var existingTodo = _repo.Get(2);
+			existingTodo.Description = newDescription;
+
+			/* Test */
+			var result = _client.Put<Todo>("api/todos", existingTodo);
+			
+			/* Assert */
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+			Assert.That(_repo.Get(2).Description, Is.EqualTo(newDescription));
+		}
+	}
+}
