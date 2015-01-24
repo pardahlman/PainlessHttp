14aa938c436c151ff7f279a0c20e6fea40ff07b2
par.dahlman <par.dahlman@valtech.se>
2015-01-24 13:24:52 +0100

    Verify method calls for fluent built serializer

--- PainlessHttp.Tests/Serializers/Custom/SerializeSettingsTests.cs
+++ PainlessHttp.Tests/Serializers/Custom/SerializeSettingsTests.cs
@@ -23,5 +23,47 @@ namespace PainlessHttp.Tests.Serializers.Custom
 			/* Assert */
 			Assert.That(result.ContentType, Is.EqualTo(expected));
 		}
+
+		[Test]
+		public void Should_Call_Provided_Serialize_Func_Upon_Serialize()
+		{
+			/* Setup */
+			var serializedCalled = false;
+			var serializer = SerializeSettings
+				.For(ContentType.ApplicationJson)
+				.Serialize(o =>
+							{
+								serializedCalled = true;
+								return string.Empty;
+							})
+				.Deserialize((s, t) => new object());
+			
+			/* Test */
+			serializer.Serialize(new object());
+
+			/* Assert */
+			Assert.That(serializedCalled, Is.True);
+		}
+
+		[Test]
+		public void Should_Call_Provided_Deserialize_Func_Upon_Deserialization()
+		{
+			/* Setup */
+			var deserializedCalled = false;
+			var serializer = SerializeSettings
+				.For(ContentType.ApplicationJson)
+				.Serialize(o => string.Empty)
+				.Deserialize((s, t) =>
+							{
+								deserializedCalled = true;
+								return new object();
+							});
+
+			/* Test */
+			serializer.Deserialize<object>(string.Empty);
+
+			/* Assert */
+			Assert.That(deserializedCalled, Is.True);
+		}
 	}
 }
