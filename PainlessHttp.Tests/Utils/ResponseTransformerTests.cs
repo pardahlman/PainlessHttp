ba827c594fb70681254c65a3878a5aa37fc6aaf6
par.dahlman <par.dahlman@valtech.se>
2015-01-24 13:38:20 +0100

    Introduce ReponseTransformer
    This will take the IHttpWebReponse and convert it to our domain object.

---
+++ PainlessHttp.Tests/Utils/ResponseTransformerTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Moq;
+using NUnit.Framework;
+using PainlessHttp.Http;
+using PainlessHttp.Serializers.Contracts;
+using PainlessHttp.Serializers.Defaults;
+using PainlessHttp.Utils;
+
+namespace PainlessHttp.Tests.Utils
+{
+	public class ResponseTransformerTests
+	{
+		private ResponseTransformer _transformer;
+		private List<IContentSerializer> _serializers;
+		private Mock<IHttpWebResponse> _rawResponse;
+
+		[SetUp]
+		public void Setup()
+		{
+			_serializers = new List<IContentSerializer>();
+			_transformer = new ResponseTransformer(_serializers);
+
+			_rawResponse = new Mock<IHttpWebResponse>();
+			_rawResponse
+				.Setup(r => r.GetResponseStream())
+				.Returns(new MemoryStream());
+		}
+
+		[TestCase(HttpStatusCode.OK)]
+		[TestCase(HttpStatusCode.PartialContent)]
+		public async void Should_Map_Http_Status_Code(HttpStatusCode code)
+		{
+			/* Setup */
+			_serializers.AddRange(ContentSerializers.Defaults);
+			_rawResponse
+				.Setup(r => r.StatusCode)
+				.Returns(code);
+
+			/* Test */
+			var result = await _transformer.TransformAsync<string>(_rawResponse.Object);
+
+			/* Assert */
+			Assert.That(result.StatusCode, Is.EqualTo(code));
+		}
+
+		[TestCase(ContentType.ApplicationJson, ContentTypes.ApplicationJson)]
+		[TestCase(ContentType.ApplicationXml, ContentTypes.ApplicationXml)]
+		[TestCase(ContentType.TextHtml, ContentTypes.TextHtml)]
+		public async void Should_Use_Serializer_Based_On_Content_Type(ContentType contentType, string contentHeaderValue)
+		{
+			/* Setup */
+			var mockedSerializers = new List<Mock<IContentSerializer>>
+			{
+				new Mock<IContentSerializer>(),
+				new Mock<IContentSerializer>(),
+				new Mock<IContentSerializer>()
+			};
+
+			mockedSerializers[0]
+				.Setup(s => s.ContentType)
+				.Returns(new List<ContentType> {ContentType.ApplicationXml});
+			mockedSerializers[1]
+				.Setup(s => s.ContentType)
+				.Returns(new List<ContentType> {ContentType.ApplicationJson});
+			mockedSerializers[2]
+				.Setup(s => s.ContentType)
+				.Returns(new List<ContentType> {ContentType.TextHtml});
+			_serializers.AddRange(mockedSerializers.Select(s => s.Object));
+
+			var usedSerializer = mockedSerializers.First(s => s.Object.ContentType.Contains(contentType));
+			usedSerializer
+				.Setup(s => s.Deserialize<object>(It.IsAny<string>()))
+				.Verifiable();
+
+			var responseHeaders = new WebHeaderCollection {{HttpResponseHeader.ContentType, contentHeaderValue}};
+			_rawResponse
+				.Setup(r => r.Headers)
+				.Returns(responseHeaders);
+
+			/* Test */
+			await _transformer.TransformAsync<object>(_rawResponse.Object);
+
+			/* Assert */
+			usedSerializer.VerifyAll();
+		}
+	}
+}
---
+++ PainlessHttp/Utils/ResponseTransformer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PainlessHttp.Http;
+using PainlessHttp.Http.Contracts;
+using PainlessHttp.Serializers.Contracts;
+
+namespace PainlessHttp.Utils
+{
+	public class ResponseTransformer
+	{
+		private readonly IEnumerable<IContentSerializer> _serializers;
+
+		public ResponseTransformer(IEnumerable<IContentSerializer> serializers)
+		{
+			_serializers = serializers;
+		}
+
+		public async Task<IHttpResponse<T>> TransformAsync<T>(IHttpWebResponse raw) where T : class 
+		{
+			var payloadTask = DeserializeAsync<T>(raw);
+			return await payloadTask.ContinueWith(task =>
+			{
+				IHttpResponse<T> result = new HttpResponse<T>
+				{
+					StatusCode = raw.StatusCode,
+					Body = task.Result
+				};
+				return result;
+			});
+		}
+
+		private async Task<T> DeserializeAsync<T>(IHttpWebResponse raw) where T : class
+		{
+
+			var contentType = ExtractContentTypeFromHeaders(raw.Headers);
+			var serializer = _serializers.FirstOrDefault(s => s.ContentType.Contains(contentType));
+			if (serializer == null)
+			{
+				throw new InstanceNotFoundException(string.Format("No registered serializer found for content type '{0}'.", contentType));
+			}
+
+			var readTask = ReadBodyAsync(raw);
+			var deserializeTask = await readTask.ContinueWith(task =>
+			{
+				if (typeof(T) == typeof(string))
+				{
+					return task.Result as T;
+				}
+				var body = task.Result;
+				var typedBody = serializer.Deserialize<T>(body);
+				return typedBody;
+			});
+			return deserializeTask;
+		}
+
+		private ContentType ExtractContentTypeFromHeaders(WebHeaderCollection headers)
+		{
+			var defaultContentType = ContentType.ApplicationJson;
+			if (headers == null)
+			{
+				return defaultContentType;
+			}
+
+			var contentTypeHeader = headers[HttpResponseHeader.ContentType];
+			if (contentTypeHeader == null)
+			{
+				return defaultContentType;
+			}
+
+			var contentType = HttpConverter.ContentType(contentTypeHeader);
+			return contentType;
+		}
+
+		public static async Task<string> ReadBodyAsync(IHttpWebResponse response)
+		{
+			var responseStream = response.GetResponseStream();
+			if (responseStream == null)
+			{
+				throw new ArgumentNullException("response");
+			}
+
+			string raw;
+			using (var reader = new StreamReader(responseStream))
+			{
+				raw = await reader.ReadToEndAsync();
+			}
+			return raw;
+		}
+	}
+}
