cd4dd2ff91f60b784b768b2376cf18b7357f5300
par.dahlman <par.dahlman@valtech.se>
2015-01-24 10:51:05 +0100

    Add integration tests for DELETE

---
+++ PainlessHttp.IntegrationTests/Methods/DeleteTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PainlessHttp.Client;
+using PainlessHttp.Client.Configuration;
+using PainlessHttp.DevServer.Data;
+using PainlessHttp.DevServer.Model;
+
+namespace PainlessHttp.IntegrationTests.Methods
+{
+	[TestFixture]
+	public class DeleteTests
+	{
+		private HttpClient _client;
+		private InMemoryTodoRepo _repo;
+
+		[SetUp]
+		public void Setup()
+		{
+			var config = new HttpClientConfiguration
+			{
+				BaseUrl = WebApiSetupFixture.BaseAddress
+			};
+
+			_client = new HttpClient(config);
+			_repo = InMemoryTodoRepo.Instance;
+		}
+
+		[Test]
+		public void Should_Be_Able_To_Perform_Delete()
+		{
+			/* Setup */
+			var todo = new Todo { Description = "Do the dishes" };
+			var saved = _repo.Add(todo);
+
+			/* Test */
+			var response = _client.Delete<string>(string.Format("api/todos/{0}", saved.Id));
+
+			/* Assert */
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(_repo.Get(saved.Id), Is.Null, "Todo should have been removed");
+		}
+
+		[Test]
+		public async void Should_Be_Able_To_Perform_Delete_Async()
+		{
+			/* Setup */
+			var todo = new Todo { Description = "Do the dishes" };
+			var saved = _repo.Add(todo);
+
+			/* Test */
+			var response = await _client.DeleteAsync<string>(string.Format("api/todos/{0}", saved.Id));
+
+			/* Assert */
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(_repo.Get(saved.Id), Is.Null, "Todo should have been removed");
+		}
+	}
+}
