307f0823b908ce0eb96e34d720c53733a7701808
par.dahlman <par.dahlman@valtech.se>
2015-01-24 12:28:44 +0100

    Verifying functionality of Serializer<T>

---
+++ PainlessHttp.Tests/Serializers/Custom/SerializerTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+using PainlessHttp.Http;
+using PainlessHttp.Serializers.Custom;
+
+namespace PainlessHttp.Tests.Serializers.Custom
+{
+	[TestFixture]
+	public class SerializerTests
+	{
+		[Test]
+		public void Should_Throw_Exception_If_Generic_Type_Does_Not_Have_Serialize_Method()
+		{
+			/* Test, Setup & Assert */
+			Assert.Throws<MissingMethodException>(() => new Serializer<NoSerializeMethod>(ContentType.Unknown));
+		}
+
+		[Test]
+		public void Should_Throw_Exception_If_Generic_Type_Does_Not_Have_Deserialize_Method()
+		{
+			/* Test, Setup & Assert */
+			Assert.Throws<MissingMethodException>(() => new Serializer<NoDeserializeMethod>(ContentType.Unknown));
+		}
+
+		[Test]
+		public void Should_Throw_Exception_If_Generic_Type_Has_Correct_Methods()
+		{
+			/* Test, Setup & Assert */
+			Assert.DoesNotThrow(() => new Serializer<CorrectMethods>(ContentType.Unknown));
+		}
+
+		[Test]
+		public void Should_Call_Correct_Methods_On_Serialize()
+		{
+			/* Setup */
+			var serializedCalled = false;
+
+			CorrectMethods.MockMethods(
+				serializeMethod =>
+				{
+					serializedCalled = true;
+					return string.Empty;
+				},
+				null
+			);
+
+			var serializer = new Serializer<CorrectMethods>(ContentType.Unknown);
+
+			/* Test */
+			serializer.Serialize(string.Empty);
+
+			/* Assert */
+			Assert.That(serializedCalled, Is.True);
+		}
+
+		[Test]
+		public void Should_Call_Correct_Methods_On_Deserialize()
+		{
+			/* Setup */
+			var deserializedCalled = false;
+
+			CorrectMethods.MockMethods(
+				null,
+				(deserializeMethod, type) =>
+				{
+					deserializedCalled = true;
+					return new object();
+				}
+			);
+
+			var serializer = new Serializer<CorrectMethods>(ContentType.Unknown);
+
+			/* Test */
+			serializer.Deserialize<object>(string.Empty);
+
+			/* Assert */
+			Assert.That(deserializedCalled, Is.True);
+		}
+	}
+}
--- PainlessHttp.Tests/Serializers/SerializerTestClasses.cs
+++ PainlessHttp.Tests/Serializers/SerializerTestClasses.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Moq;
 using NUnit.Framework;
 
 namespace PainlessHttp.Tests.Serializers
@@ -13,5 +17,62 @@ namespace PainlessHttp.Tests.Serializers
 		public int IntProp { get; set; }
 		public List<string> ListProp { get; set; }
 		public SerializerTestClass ObjectRef { get; set; }
+	}
+
+	/// <summary>
+	/// Test class that has a Deserialize method, but no serialize method.
+	/// This class is invalid as a Serializer generic argument.
+	/// </summary>
+	public class NoSerializeMethod
+	{
+		public static object Deserialize(string data, Type type)
+		{
+			return new object();
+		}
+	}
+
+	/// <summary>
+	/// Test class that has a Deserialize method, but no serialize method.
+	/// This class is invalid as a Serializer generic argument.
+	/// </summary>
+	public class NoDeserializeMethod
+	{
+		public static string Serialize(object data)
+		{
+			return string.Empty;
+		}
+	}
+
+	/// <summary>
+	///  Test class that has the correct method signature, and
+	/// therefore is accepted as generic parameter to Serializer.
+	/// </summary>
+	public class CorrectMethods
+	{
+		private static Func<object, string> _serialize;
+		private static Func<string, Type, object> _deserialize;
+
+		public static void MockMethods(Func<object, string> mockSerialize, Func<string, Type, object> mockDeserialize)
+		{
+			_serialize = mockSerialize;
+			_deserialize = mockDeserialize;
+		}
+		public static string Serialize(object data)
+		{
+			if (_serialize == null)
+			{
+				return string.Empty;
+			}
+			return _serialize(data);
+		}
+
+		public static object Deserialize(string data, Type type)
+		{
+			if (_deserialize == null)
+			{
+				return new object();
+			}
+			return _deserialize(data, type);
+		}
 	}
 }
