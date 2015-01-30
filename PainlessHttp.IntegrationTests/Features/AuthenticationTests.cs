1bc582888d487460aaf188e8fe12648812bc0415
par.dahlman <par.dahlman@valtech.se>
2015-01-30 19:58:13 +0100

    DG-20: Adding Authentication test
    That currently fails. Time to refactor this :)

---
+++ PainlessHttp.IntegrationTests/Features/AuthenticationTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using NUnit.Framework;
+using PainlessHttp.Client;
+using PainlessHttp.Client.Configuration;
+
+namespace PainlessHttp.IntegrationTests.Features
+{
+	[TestFixture]
+	public class AuthenticationTests
+	{
+		private HttpClient _client;
+
+		[SetUp]
+		public void Setup()
+		{
+			var config = new HttpClientConfiguration
+			{
+				BaseUrl = WebApiSetupFixture.BaseAddress
+			};
+
+			_client = new HttpClient(config);
+		}
+
+		[Test]
+		public void Should_Return_Unauthorized_If_No_Credentials_Provided()
+		{
+			/* Setup */
+			/* Test */
+			var response = _client.Get<string>("api/authentication");
+
+			/* Assert */
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+		}
+	}
+}
