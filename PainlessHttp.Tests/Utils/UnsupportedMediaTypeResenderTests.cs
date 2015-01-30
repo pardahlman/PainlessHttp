22b98d6fa698fc4afbb1fec42a866c93e61e3a29
par.dahlman <par.dahlman@valtech.se>
2015-01-30 22:17:22 +0100

    Adding UnsupportedMediaTypeResender
    Which is a class that can analyze an error response and see if it can act
    on it. Currently it only contains a IsApplicable method, but it will also
    help prepare the request later.

---
+++ PainlessHttp.Tests/Utils/UnsupportedMediaTypeResenderTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using NUnit.Framework;
+using PainlessHttp.Serializers.Contracts;
+using PainlessHttp.Serializers.Typed;
+using PainlessHttp.Utils;
+using HttpWebResponse = PainlessHttp.Utils.HttpWebResponse;
+
+namespace PainlessHttp.Tests.Utils
+{
+	[TestFixture]
+	public class UnsupportedMediaTypeResenderTests
+	{
+		private UnsupportedMediaTypeResender _resender;
+		private HttpWebResponse _response;
+		private List<IContentSerializer> _serializers;
+
+		[SetUp]
+		public void Setup()
+		{
+			_serializers = new List<IContentSerializer>
+			{
+				new DefaultJsonSerializer(),
+				new DefaultXmlSerializer()
+			};
+			_resender = new UnsupportedMediaTypeResender(_serializers);
+			_response = new HttpWebResponse
+			{
+				Headers = new WebHeaderCollection
+						{
+							{HttpRequestHeader.Accept, "application/json"}
+						},
+				StatusCode = HttpStatusCode.UnsupportedMediaType
+			};
+		}
+
+		public class When_Calling_IsApplicable : UnsupportedMediaTypeResenderTests
+		{
+			[Test]
+			public void Should_Be_False_If_Response_Is_Null()
+			{
+				/* Setup */
+				/* Test */
+				var result = _resender.IsApplicable(null);
+
+				/* Assert */
+				Assert.That(result, Is.False);
+			}
+
+			[Test]
+			public void Should_Be_True_If_Response_Has_Unsupported_Media_Type_And_Accept_Header_With_Supported_Type()
+			{
+				/* Setup */
+				//above
+
+				/* Test */
+				var result = _resender.IsApplicable(_response);
+
+				/* Assert */
+				Assert.That(result, Is.True);
+			}
+
+			[Test]
+			public void Should_Be_False_If_No_Accept_Headers_In_Response()
+			{
+				/* Setup */
+				_response.Headers = new WebHeaderCollection();
+
+				/* Test */
+				var result = _resender.IsApplicable(_response);
+
+				/* Assert */
+				Assert.That(result, Is.False);
+			}
+
+			[TestCase(HttpStatusCode.NoContent)]
+			[TestCase(HttpStatusCode.Unauthorized)]
+			[TestCase(HttpStatusCode.Forbidden)]
+			public void Should_Be_False_If_HttpStatusCode_Is_Not_UnsupportedMediaType(HttpStatusCode code)
+			{
+				/* Setup */
+				_response.StatusCode = code;
+
+				/* Test */
+				var result = _resender.IsApplicable(_response);
+
+				/* Assert */
+				Assert.That(result, Is.False);
+			}
+
+			[Test]
+			public void Should_Be_False_If_No_Content_Type_Is_Supported()
+			{
+				/* Setup */
+				_response.Headers = new WebHeaderCollection
+				{
+					{HttpRequestHeader.Accept, "application/unsupported"}
+				};
+
+				/* Test */
+				var result = _resender.IsApplicable(_response);
+
+				/* Assert */
+				Assert.That(result, Is.False);
+			}
+		}
+
+
+	}
+}
---
+++ PainlessHttp/Utils/UnsupportedMediaTypeResender.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using PainlessHttp.Serializers.Contracts;
+
+namespace PainlessHttp.Utils
+{
+	public class UnsupportedMediaTypeResender : IRequestResender
+	{
+		private readonly List<IContentSerializer> _serializers;
+		private readonly AcceptHeaderMapper _mapper;
+
+		public UnsupportedMediaTypeResender(List<IContentSerializer> serializers)
+		{
+			_mapper = new AcceptHeaderMapper();
+			_serializers = serializers;
+		}
+
+		public bool IsApplicable(IHttpWebResponse response)
+		{
+			if (response == null)
+			{
+				return false;
+			}
+
+			if (response.StatusCode != HttpStatusCode.UnsupportedMediaType)
+			{
+				return false;
+			}
+
+			var acceptHeader = response.Headers[HttpRequestHeader.Accept];
+			if (string.IsNullOrWhiteSpace(acceptHeader))
+			{
+				return false;
+			}
+
+			var headers  = _mapper.Map(acceptHeader);
+			var contentType = headers.Select(h => HttpConverter.ContentTypeOrDefault(h.ContentType));
+
+			var serializer = _serializers
+				.FirstOrDefault(s =>
+					contentType.Any(ct => s.ContentType.Contains(ct))
+				);
+
+			if (serializer == null)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+
+	public interface IRequestResender
+	{
+		bool IsApplicable(IHttpWebResponse response);
+	}
+}
