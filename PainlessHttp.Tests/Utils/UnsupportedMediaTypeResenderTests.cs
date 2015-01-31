4c9655f4d566b726b4be92d592cfa815bda7dcff
par.dahlman <par.dahlman@valtech.se>
2015-01-31 12:53:00 +0100

    Implement 'Resend' for UnsupportedMediaResender
    So that it can try to resend it with a correct content type.

--- PainlessHttp.Tests/Utils/UnsupportedMediaTypeResenderTests.cs
+++ PainlessHttp.Tests/Utils/UnsupportedMediaTypeResenderTests.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Net;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
+using PainlessHttp.Http;
+using PainlessHttp.Http.Contracts;
 using PainlessHttp.Serializers.Contracts;
 using PainlessHttp.Serializers.Typed;
 using PainlessHttp.Utils;
@@ -14,16 +20,23 @@ namespace PainlessHttp.Tests.Utils
 		private UnsupportedMediaTypeResender _resender;
 		private HttpWebResponse _response;
 		private List<IContentSerializer> _serializers;
+		private Mock<IWebRequestWorker> _worker;
+		private WebRequestSpecifications _reqSpecs;
 
 		[SetUp]
 		public void Setup()
 		{
+			_worker = new Mock<IWebRequestWorker>();
 			_serializers = new List<IContentSerializer>
 			{
 				new DefaultJsonSerializer(),
 				new DefaultXmlSerializer()
 			};
-			_resender = new UnsupportedMediaTypeResender(_serializers);
+			_resender = new UnsupportedMediaTypeResender(_serializers, _worker.Object);
+			_reqSpecs = new WebRequestSpecifications
+			{
+				ContentNegotiation = true
+			};
 			_response = new HttpWebResponse
 			{
 				Headers = new WebHeaderCollection
@@ -105,6 +118,72 @@ namespace PainlessHttp.Tests.Utils
 			}
 		}
 
+		public class When_Calling_ResendRequestAsync : UnsupportedMediaTypeResenderTests
+		{
+			[Test]
+			public void Should_Throw_Exception_If_No_Serializer_Found()
+			{
+				/* Setup */
+				_response.Headers = new WebHeaderCollection
+				{
+					{HttpRequestHeader.Accept, "application/unsupported"}
+				};
 
+				/* Test & Setup */
+				Assert.Throws<ArgumentException>(async () =>  await _resender.ResendRequestAsync(_response, _reqSpecs));
+			}
+
+			[Test]
+			public void Should_Update_Content_Type_Based_On_Accept_Headers()
+			{
+				/* Setup */
+				_reqSpecs.ContentType = ContentType.TextCsv;
+				_worker
+					.Setup(w => w.GetResponseAsync(
+						It.Is<WebRequestSpecifications>(spec => spec.ContentType == ContentType.ApplicationJson)))
+					.Verifiable();
+
+				/* Test */
+				_resender.ResendRequestAsync(_response, _reqSpecs);
+
+				/* Assert */
+				_worker.VerifyAll();
+			}
+
+			[Test]
+			public async void Should_Update_Data_Serialization_Method_Based_Out_Accept_Headers()
+			{
+				/* Setup */
+				_reqSpecs.Data = new AcceptHeaderField{ ContentType = "sometype"};
+				var expectedSerialized = new DefaultJsonSerializer().Serialize(_reqSpecs.Data);
+				var actualSerialized = string.Empty;
+				_worker
+					.Setup(w => w.GetResponseAsync(It.IsAny<WebRequestSpecifications>()))
+					.Callback((WebRequestSpecifications w) => actualSerialized = w.SerializeData());
+				
+				/* Test */
+				_resender.ResendRequestAsync(_response, _reqSpecs);
+
+				/* Assert */
+				Assert.That(actualSerialized, Is.EqualTo(expectedSerialized));
+			}
+
+			[Test]
+			public void Should_Not_Resend_Request_If_Content_Negotiation_Is_Disabled()
+			{
+				/* Setup */
+				_reqSpecs.ContentNegotiation = false;
+				Expression<Action<IWebRequestWorker>> performRequest = worker => worker.GetResponseAsync(It.IsAny<WebRequestSpecifications>());
+				_worker
+					.Setup(performRequest)
+					.Verifiable("");
+
+				/* Test */
+				_resender.ResendRequestAsync(_response, _reqSpecs);
+
+				/* Assert */
+				_worker.Verify(performRequest, Times.Never);
+			}
+		}
 	}
 }
--- PainlessHttp/Utils/UnsupportedMediaTypeResender.cs
+++ PainlessHttp/Utils/UnsupportedMediaTypeResender.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
+using PainlessHttp.Http;
 using PainlessHttp.Serializers.Contracts;
 
 namespace PainlessHttp.Utils
@@ -8,12 +11,14 @@ namespace PainlessHttp.Utils
 	public class UnsupportedMediaTypeResender : IRequestResender
 	{
 		private readonly List<IContentSerializer> _serializers;
+		private readonly IWebRequestWorker _worker;
 		private readonly AcceptHeaderMapper _mapper;
 
-		public UnsupportedMediaTypeResender(List<IContentSerializer> serializers)
+		public UnsupportedMediaTypeResender(List<IContentSerializer> serializers, IWebRequestWorker worker)
 		{
 			_mapper = new AcceptHeaderMapper();
 			_serializers = serializers;
+			_worker = worker;
 		}
 
 		public bool IsApplicable(IHttpWebResponse response)
@@ -28,19 +33,13 @@ namespace PainlessHttp.Utils
 				return false;
 			}
 
-			var acceptHeader = response.Headers[HttpRequestHeader.Accept];
-			if (string.IsNullOrWhiteSpace(acceptHeader))
+			var acceptTypes = GetContentTypeFromAcceptHeader(response).ToList();
+			if (!acceptTypes.Any())
 			{
 				return false;
 			}
 
-			var headers  = _mapper.Map(acceptHeader);
-			var contentType = headers.Select(h => HttpConverter.ContentTypeOrDefault(h.ContentType));
-
-			var serializer = _serializers
-				.FirstOrDefault(s =>
-					contentType.Any(ct => s.ContentType.Contains(ct))
-				);
+			var serializer = GetApplicableSerializer(acceptTypes);
 
 			if (serializer == null)
 			{
@@ -48,10 +47,65 @@ namespace PainlessHttp.Utils
 			}
 			return true;
 		}
+
+		public async Task<IHttpWebResponse> ResendRequestAsync(IHttpWebResponse response, WebRequestSpecifications specs)
+		{
+			if (!specs.ContentNegotiation)
+			{
+				return response;
+			}
+
+			ContentType supportedType;
+			var acceptTypes = GetContentTypeFromAcceptHeader(response).ToList();
+			var serializer = GetApplicableSerializer(acceptTypes, out supportedType);
+			if (serializer == null)
+			{
+				throw new ArgumentException("Can not find serializer for the type(s) " + acceptTypes.Select(t => string.Format("{0} ", t.ToString())));
+			}
+			specs.ContentType = supportedType;
+			specs.SerializeData = () => serializer.Serialize(specs.Data);
+			var newResponse = await _worker.GetResponseAsync(specs);
+			return newResponse;
+		}
+
+		private IContentSerializer GetApplicableSerializer(IEnumerable<ContentType> contentTypes)
+		{
+			ContentType type;
+			return GetApplicableSerializer(contentTypes, out type);
+		}
+
+		private IContentSerializer GetApplicableSerializer(IEnumerable<ContentType> contentTypes, out ContentType found)
+		{
+			var supportedType = contentTypes.FirstOrDefault(type => _serializers.Any(ser => ser.ContentType.Contains(type)));
+			found = supportedType;
+			if (supportedType == ContentType.Unknown)
+			{
+				return null;
+			}
+
+			var serializer = _serializers.First(s => contentTypes.Any(ct => ct == supportedType));
+			return serializer;
+		}
+
+		private IEnumerable<ContentType> GetContentTypeFromAcceptHeader(IHttpWebResponse response)
+		{
+			var acceptHeader = response.Headers["Accept"];
+			if (acceptHeader == null)
+			{
+				yield break;
+			}
+
+			var headers = _mapper.Map(acceptHeader);
+			foreach (var header in headers)
+			{
+				yield return HttpConverter.ContentTypeOrDefault(header.ContentType);
+			}
+		}
 	}
 
 	public interface IRequestResender
 	{
 		bool IsApplicable(IHttpWebResponse response);
+		Task<IHttpWebResponse> ResendRequestAsync(IHttpWebResponse response, WebRequestSpecifications specs);
 	}
 }
