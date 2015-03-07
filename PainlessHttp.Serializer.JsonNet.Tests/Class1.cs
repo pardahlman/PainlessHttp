664ccde3d434f4f0833b542de15e2dcc11b4147f
par.dahlman <par.dahlman@valtech.se>
2015-03-07 10:34:51 +0100

    Add JsonSerializer with interface
    I've decided that we're not losing anything be refering PainlessHttp in the
    JsonSerializer project. And by doing this, we can make the JsonNet serializer
    implement the interface.

---
+++ PainlessHttp.Serializer.JsonNet.Tests/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PainlessHttp.DevServer.Model;
+
+namespace PainlessHttp.Serializer.JsonNet.Tests
+{
+	[TestFixture]
+	public class Class1
+	{
+		private string _payloadAsString;
+		private Todo _payload;
+
+		[SetUp]
+		public void Setup()
+		{
+			_payload = new Todo
+			{
+				Description = "Write a new JsonNet serialzer",
+				Id = 1,
+				IsCompleted = true
+			};
+
+			_payloadAsString = JsonConvert.SerializeObject(_payload);
+		}
+
+		[Test]
+		public void Should_Serialize_Correctly_With_Default_Settings()
+		{
+			/* Setup */
+			var serializer = new PainlessJsonNet();
+
+			/* Test */
+			var result = serializer.Serialize(_payload);
+
+			/* Assert */
+			Assert.That(result, Contains.Substring("{\"Id\":1,\"Description\":\"Write a new JsonNet serialzer\",\"IsCompleted\":true}"));
+		}
+
+		[Test]
+		public void Should_Deserialize_Correctly_With_Default_Settings()
+		{
+			/* Setup */
+			var serializer = new PainlessJsonNet();
+
+			/* Test */
+			var result = serializer.Deserialize<Todo>(_payloadAsString);
+
+			/* Assert */
+			Assert.That(result.Description, Is.EqualTo(_payload.Description));
+			Assert.That(result.Id, Is.EqualTo(_payload.Id));
+			Assert.That(result.IsCompleted, Is.EqualTo(_payload.IsCompleted));
+		}
+
+		[Test]
+		public void Should_Use_Overrides_If_Supplied()
+		{
+			/* Setup */
+			var deserializedCalled = false;
+			var serializedCalled = false;
+			Func<object, string> serialize = o =>
+			{
+				serializedCalled = true;
+				return string.Empty;
+			};
+			Func<string, Type, object> deserialize = (s, type) =>
+			{
+				deserializedCalled = true;
+				return null;
+			};
+			var serializer = new PainlessJsonNet(serialize, deserialize);
+
+			/* Test */
+			serializer.Serialize(_payload);
+			serializer.Deserialize<Todo>(_payloadAsString);
+
+			/* Assert */
+			Assert.That(deserializedCalled, Is.True);
+			Assert.That(serializedCalled, Is.True);
+		}
+	}
+}
---
+++ PainlessHttp.Serializer.JsonNet/PainlessJsonNet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using PainlessHttp.Http;
+using PainlessHttp.Serializers.Contracts;
+
+namespace PainlessHttp.Serializer.JsonNet
+{
+	public class PainlessJsonNet : IContentSerializer
+	{
+		private readonly Func<object, string> _serialize;
+		private readonly Func<string, Type, object> _deserialize;
+
+		public PainlessJsonNet(Func<object,string> serialize = null, Func<string, Type, object> deserialize = null  )
+		{
+			_serialize = serialize ?? JsonConvert.SerializeObject;
+			_deserialize = deserialize ?? JsonConvert.DeserializeObject;
+		}
+		public IEnumerable<ContentType> ContentType { get; private set; }
+		
+		public string Serialize(object data)
+		{
+			return _serialize(data);
+		}
+
+		public T Deserialize<T>(string data)
+		{
+			var result = _deserialize(data, typeof (T));
+			return (T)result;
+		}
+	}
+}
